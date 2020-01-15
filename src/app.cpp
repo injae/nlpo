fc1b00cb3477f064394189df438e70edf099cd2b
injae <8687lee@gmail.com>
2020-01-16 02:45:50 +0900

    bugfix

--- src/app.cpp
+++ src/app.cpp
@@ -6,6 +6,7 @@
 namespace nlpo
 {
     App::App() {
+        name_ = "";
         add_option("help")
         .abbr("h")
         .desc("show cppm commands and options")
@@ -48,10 +49,10 @@ namespace nlpo
     }
 
     void App::show_help() {
-        //fmt::print("Usage:\n");
-        //auto has_cmd = commands_.empty() ? "" : " <command>";
-        //auto has_opt = options_.empty() ? "" : " [--verbose]";
-        //fmt::print("{}{}{}\n",name_, has_cmd, has_opt);
+        fmt::print("Usage:\n");
+        auto has_cmd = commands_.empty() ? "" : " <command>";
+        auto has_opt = options_.empty() ? "" : " [--verbose]";
+        fmt::print("{}{}{}\n",name_, has_cmd, has_opt);
 
         if(!options_.empty()) {
             fmt::print("Option:\n");
