eec9bebdce415108ab33cadb4f2bc2ac7d3ef8d6
injae <8687lee@gmail.com>
2020-11-23 02:19:48 +0900

    fmt version update and link type change

--- include/nlpo/app.h
+++ include/nlpo/app.h
@@ -10,8 +10,7 @@
 #include "nlpo/option.h"
 #include "nlpo/command.h"
 #include "nlpo/arg.h"
-#include <fmt/format.h>
-using namespace fmt::literals;
+#include <iostream>
 
 namespace nlpo
 {
@@ -29,19 +28,19 @@ namespace nlpo
         Command& add_command(const std::string& name, App& app);
         template<typename T>
         Command& add_command(const std::string name) {
-            return add_command(name).call_back([&,name](){  T().app().name("{} {}"_format(name_, name)).parse(*this); });
+            return add_command(name).call_back([&,name](){  T().app().name(name_ + " " +name).parse(*this); });
         }
         Command& add_command();
         void call_default() { default_command_->run(); }
         std::list<std::string>& args() { return args_; }
         std::list<std::string> require_args() {
-            if(args_.empty()) {fmt::print(stderr, "No arguments\n"); exit(1);}
+            if(args_.empty()) { std::cerr << "No arguments\n"; exit(1);}
             auto args = args_;
             args_.clear();
             return args;
         }
         std::string require_arg() {
-            if(args_.empty()) {fmt::print(stderr, "No argument\n"); exit(1);}
+            if(args_.empty()) { std::cerr << "No argument\n"; exit(1);}
             return get_arg();
         }
         std::string get_arg() {auto arg = args_.front(); args_.pop_front(); return arg;}
